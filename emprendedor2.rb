0bb1c0be59aac06daa9636c41d874844ce14308a
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-19 15:03:58 -0400

    Added the control flow for emprendedor2

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -8,3 +8,8 @@ costs = ARGV[4].to_i
 #This is the profit expression including the new variables for this script
 
 profit = (price*users)+(price*2*premium_users)-costs #Here i didn't cinclude free users because they don't add to the overall profit.
+
+#This is the control flow, it applies a tax of 35% if the overall profit is a positive number.
+if profit > 0
+    profit -= profit*0.35
+end
