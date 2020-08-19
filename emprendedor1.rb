8c3ae368f416e583da8c17061e7bcddcb68e742b
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-19 14:51:35 -0400

    Added control flow

--- emprendedor1.rb
+++ emprendedor1.rb
@@ -5,3 +5,8 @@ costs = ARGV[2].to_i
 
 #This is the expression that allow us to estimate the overall profit.
 profit = price * users - costs
+
+#This is the control flow, it applies a tax of 35% if the overall profit is a positive number.
+if profit > 0
+    profit -= profit*0.35
+end
